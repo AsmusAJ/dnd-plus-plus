46f2f38460de8aa89e50aa2c69756e73594efb6e
AsmusAJ <asmusaj@umich.edu>
2025-12-04 12:24:46 -0500

    make headers editable

--- ttrpg/api/save_on_unload.py
+++ ttrpg/api/save_on_unload.py
@@ -6,17 +6,22 @@ import ttrpg
 def save_on_unload():
     data = flask.request.get_json()
     texts = data.get('texts', [])
+    headers = data.get('headers', [])
 
     # Save to SQLite
     conn = ttrpg.model.get_db()
     cursor = conn.cursor()
+
+    for header in header:
+        boxId = header['id']
+        title = header['title']
+        cursor.execute("UPDATE Boxes SET box_title=? WHERE box_id=?", (title, boxId))
 
     for text in texts:
         textId = text['id']
         textContent = text['text']
         cursor.execute("UPDATE Texts SET text_content=? WHERE text_id=?", (textContent, textId))
     conn.commit()
-    conn.close()
 
     #We use this because page is unloaded so user cant use response anyway
     return '', 204  
